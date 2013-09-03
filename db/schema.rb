aca0bc2edacaf46690ea7cbda0f9f3c6dbc9f083
jorgevaldivia <jorge+dev@valdivia.me>
2013-09-02 23:32:47 -0500

    add lat and lon fields to incidents

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130903021404) do
+ActiveRecord::Schema.define(version: 20130903043139) do
 
   create_table "incidents", force: true do |t|
     t.string   "address_1"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20130903021404) do
     t.datetime "occured_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
